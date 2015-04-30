696f80204b9b2fe7b3d03de72ff3a6bb16d18258
tsanie <tsanie.lily@gmail.com>
2015-04-30 17:37:54 +0800

    version

--- ElectronicObserver/Window/Dialog/DialogVersion.cs
+++ ElectronicObserver/Window/Dialog/DialogVersion.cs
@@ -15,7 +15,7 @@ namespace ElectronicObserver.Window.Dialog {
 		public DialogVersion() {
 			InitializeComponent();
 
-			this.Text = "魔改版本 - 429.1427";
+			this.Text = "魔改版本 - 430.1735";
 			TextVersion.Text = string.Format( "{0} (ver. {1} - {2} Release)", SoftwareInformation.VersionJapanese, SoftwareInformation.VersionEnglish, SoftwareInformation.UpdateTime.ToString( "d" ) ); 
 		}
 

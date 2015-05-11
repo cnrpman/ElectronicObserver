edd676c1050b9c5149c63af9a56c500f2555c800
rpman <thinkinginjavadawn@gmail.com>
2015-05-11 18:06:57 +0800

    界面修改

--- ElectronicObserver/Window/Dialog/DialogResourcesGraph.cs
+++ ElectronicObserver/Window/Dialog/DialogResourcesGraph.cs
@@ -31,15 +31,28 @@ namespace ElectronicObserver.Window.Dialog
             }
         }
 
+        private int day2tbfloor(double day)
+        {
+            return (int)(Math.Sqrt(day) * 10);
+        }
+
+        private int day2tbceil(double day)
+        {
+            return (int)Math.Ceiling(Math.Sqrt(day) * 10);
+        }
+
         private void loadResChart()
         {
-            System.DateTime mindt = System.DateTime.Today;
+            System.DateTime mindt = System.DateTime.Now;
+            System.DateTime maxdt = new System.DateTime(1, 1, 1);
             List<string[]> dat=ReadCSV( System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase+"Record\\ResourceRecord.csv");
             for (int i = 0; i < dat.Count; i++)
             {
                 System.DateTime parsedt = System.DateTime.Parse(dat[i][0]);
                 if (parsedt < mindt)
                     mindt = parsedt;
+                if (parsedt > maxdt)
+                    maxdt = parsedt;
                 double dt=parsedt.ToOADate();
 
                 
@@ -49,10 +62,24 @@ namespace ElectronicObserver.Window.Dialog
                 this.chart1.Series[3].Points.Add(new DataPoint(dt, dat[i][4]));
                 this.chart1.Series[4].Points.Add(new DataPoint(dt, dat[i][6]));
             }
-            int maxdays = (int)Math.Ceiling((System.DateTime.Today - mindt).TotalDays);
-            maxdays = Math.Max(maxdays, 2);
-            trackBarDate.Maximum = maxdays;
-            trackBarDate.Value = maxdays;
+            double maxdays = (System.DateTime.Now - mindt).TotalDays;
+            trackBarDate.Maximum = day2tbceil(maxdays);//the farest day
+
+            double mindays = (System.DateTime.Now - maxdt).TotalDays;
+            trackBarDate.Minimum = day2tbceil(mindays);//nearest
+
+
+            this.chart1.ChartAreas[0].AxisX.Maximum = System.DateTime.Now.ToOADate();
+            if ((int)Math.Ceiling((System.DateTime.Now - maxdt).TotalDays) > 30)//if the nearly log is over 30 days
+            {
+                trackBarDate.Value = (trackBarDate.Maximum + trackBarDate.Minimum) >> 1;
+                this.chart1.ChartAreas[0].AxisX.Minimum = (System.DateTime.Now - new System.TimeSpan((int)Math.Ceiling((double)trackBarDate.Value * trackBarDate.Value / 100), 0, 0, 0)).ToOADate();
+                return;
+            }
+
+            double nowdays = Math.Min(maxdays, 36.0);//show chart in 36 days firstly
+            trackBarDate.Value = day2tbceil(nowdays);
+            this.chart1.ChartAreas[0].AxisX.Minimum = (System.DateTime.Now - new System.TimeSpan((int)Math.Ceiling((double)trackBarDate.Value * trackBarDate.Value / 100), 0, 0, 0)).ToOADate();
         }
 
         public static List<String[]> ReadCSV(string filePathName)
@@ -81,7 +108,7 @@ namespace ElectronicObserver.Window.Dialog
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
             TrackBar tb = sender as TrackBar;
-            this.chart1.ChartAreas[0].AxisX.Minimum = (System.DateTime.Today - new System.TimeSpan(tb.Value, 0, 0, 0)).ToOADate();
+            this.chart1.ChartAreas[0].AxisX.Minimum = (System.DateTime.Now - new System.TimeSpan((int)Math.Ceiling((double)tb.Value*tb.Value/100), 0, 0, 0)).ToOADate();
         }
     }
 }
